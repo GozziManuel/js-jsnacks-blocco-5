f69b390b20e67445fb66e6f099494900278e5842
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-01-21 15:58:47 +0100

    snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,7 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+console.log(numbers);
+const numberPlusOne = [...numbers];
+const sumPlusOne = numberPlusOne.map((number) => number + 1);
+console.log(sumPlusOne);
